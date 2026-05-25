ba082f4b227c02ead659d30cc32524c1c778df4f
rishab raj verma <rishabrajv@mindfiresolutions.com>
2026-05-25 19:06:00 +0530

    feat: add populate query for header and footer api

---
+++ src/api/globle/controllers/globle.ts
@@ -0,0 +1,7 @@
+/**
+ * globle controller
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreController('api::globle.globle');
---
+++ src/api/globle/routes/globle.ts
@@ -0,0 +1,13 @@
+/**
+ * globle router
+ */
+
+import { factories } from '@strapi/strapi';
+
+export default factories.createCoreRouter('api::globle.globle',{
+   config:{
+   find:{
+     middlewares:["api::globle.global-papulate"]
+   }
+  }
+});
