e4565dc7d8e21ca74bdadb368515ca723e95d852
rishab raj verma <rishabrajv@mindfiresolutions.com>
2026-06-05 10:10:03 +0530

    fix: remove CORS issue configure

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -4,20 +4,7 @@ const config: Core.Config.Middlewares = [
   'strapi::logger',
   'strapi::errors',
   'strapi::security',
-  {
-    name: 'strapi::cors',
-    config: {
-      enabled: true,
-      // Add your Vercel frontend URL(s) here
-      origin: [
-        'https://contentful-cms-app-mu.vercel.app',
-        'http://localhost:3000',
-      ],
-      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'HEAD', 'OPTIONS'],
-      headers: ['Content-Type', 'Authorization', 'Origin', 'Accept'],
-      keepHeaderOnError: true,
-    },
-  },
+  'strapi::cors',
   'strapi::poweredBy',
   'strapi::query',
   'strapi::body',
